ce119886c978714dc08ace650cbb6e5c66755b35
NickWatt1992 <J221822@tafe.wa.edu.au>
2018-04-28 13:55:02 +0800

    Add files via upload
    Version 1.1

--- Flight Seating Application/src/flight/seating/application/FXMLDocumentController.java
+++ Flight Seating Application/src/flight/seating/application/FXMLDocumentController.java
@@ -6,6 +6,7 @@
 package flight.seating.application;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,7 +15,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import java.util.Arrays;
+import java.util.List;
 import javafx.application.Platform;
+import javafx.scene.control.TextField;
 import javax.swing.JOptionPane;
 
 /**
@@ -22,6 +25,10 @@ import javax.swing.JOptionPane;
  * @author Nick
  */
 public class FXMLDocumentController implements Initializable {
+    
+    private Customer currentCustomer;
+
+    private FlightSeatingApplication fsa;
     
     @FXML
     private Button seatingButton;
@@ -50,11 +57,17 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button exitButton;
     
-    private FlightSeatingApplication fsa;
+    @FXML
+    public TextField firstName;
+    @FXML
+    public TextField lastName;
+    @FXML
+    private TextField searchCustomer;
     
     @FXML
     private void handleSeatingButtonAction(ActionEvent event) {
 
+        
         seatingTextArea.setText(fsa.ShowSeating());
     }
     
@@ -62,10 +75,70 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
        fsa = new FlightSeatingApplication();
+       fsa.setupSeats();
+       
+       newCustomer();
     } 
-
+    
     @FXML
     private void handleExitButtonAction(ActionEvent event) {
         Platform.exit();
     }
+    
+     @FXML
+    private void handleConfirmButtonAction(ActionEvent event) {
+        currentCustomer.setName(firstName.getText() + " " + lastName.getText());
+        fsa.customerList.add(currentCustomer);
+        fsa.SeatAllocation(currentCustomer);
+        // Add a confirmation message
+        newCustomer();
+    }
+    
+    @FXML
+    private void handleAdultButtonAction(ActionEvent event) {
+        currentCustomer.setAgeType("Adult");
+    }
+    @FXML
+    private void handleChildButtonAction(ActionEvent event) {
+        currentCustomer.setAgeType("Child");
+    }
+    @FXML
+    private void handleFirstClassButtonAction(ActionEvent event) {
+        currentCustomer.setClassType("First");
+    }
+    @FXML
+    private void handleBusinessClassButtonAction(ActionEvent event) {
+        currentCustomer.setClassType("Business");
+    }
+    @FXML
+    private void handleEconomyClassButtonAction(ActionEvent event) {
+        currentCustomer.setClassType("Economy");
+    }
+    @FXML
+    private void handleWindowButtonAction(ActionEvent event) {
+        currentCustomer.setSeatType("Window");
+    }
+    @FXML
+    private void handleAisleButtonAction(ActionEvent event) {
+        currentCustomer.setSeatType("Aisle");
+    }
+    @FXML
+    private void handleMiddleButtonAction(ActionEvent event) {
+        currentCustomer.setSeatType("Middle");
+    }
+    @FXML
+    private void handleCancelButtonAction(ActionEvent event) {
+        
+    }
+
+    private void setAgeType(String ageType)
+    {
+        currentCustomer.setAgeType(ageType);
+    }
+   
+    private void newCustomer()
+    {
+        currentCustomer = new Customer();
+    }
+    
 }
--- Flight Seating Application/src/flight/seating/application/FlightSeatingApplication.java
+++ Flight Seating Application/src/flight/seating/application/FlightSeatingApplication.java
@@ -5,18 +5,29 @@
  */
 package flight.seating.application;
 
+import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import static javafx.scene.input.KeyCode.T;
 
 /**
  *
  * @author Nick
  */
 public class FlightSeatingApplication extends Application {
+    
+    public FXMLDocumentController fc = new FXMLDocumentController();
+    List<Customer> customerList = new ArrayList<Customer>();;
+    List<Seat> seatsList = new ArrayList<Seat>();
     
     @Override
     public void start(Stage stage) throws Exception {
@@ -26,6 +37,8 @@ public class FlightSeatingApplication extends Application {
         
         stage.setScene(scene);
         stage.show();
+        
+        setupSeats();
     }
 
     /**
@@ -35,31 +48,142 @@ public class FlightSeatingApplication extends Application {
         launch(args);
     }    
     
-    public String ShowSeating(){
-        String seats[][] = {
+    public void setupSeats()
+    {
+        String seatType = null;
+        String seatClass;
+        int countRow = 0;
+        int rowNumber = 0;
+        int seatNumber = 0;
+        for(int i = 0; i < 72; i++)
+        {          
+
+            seatNumber = i - (6*(rowNumber-1));
+            seatNumber = seatNumber + 1;
             
-            {"-","-","-"," ","-","-","-"},
-            {"-","-","-"," ","-","-","-"},
-            {"-","-","-"," ","-","-","-"},
-            {"-","-","-"," ","-","-","-"},
-            {"-","-","-"," ","-","-","-"},
-            {"-","-","-"," ","-","-","-"},
-            {"-","-","-"," ","-","-","-"},
-            {"-","-","-"," ","-","-","-"},
-            {"-","-","-"," ","-","-","-"},
-            {"-","-","-"," ","-","-","-"},
-            {"-","-","-"," ","-","-","-"},
-            {"-","-","-"," ","-","-","-"},
-            {"-","-","-"," ","-","-","-"}
-        };
-        String txt = "";
-            for(int i = 0; i < seats.length; i++){
-                for(int j = 0; j <seats[i].length; j++){
-                    txt = txt + seats[i][j] + " , ";
-                }
-                txt = txt + "\n";
+            if(i % 6 == 0)
+            {
+                rowNumber = rowNumber + 1;
             }
             
-         return txt;
+            if(seatNumber == 7)
+            {
+                seatNumber = 1;
+            }
+            
+            switch (seatNumber)
+            {
+                case 1:
+                    seatType = "Window";
+                    break;
+                case 3:
+                    seatType = "Aisle";
+                    break;
+                case 4:
+                    seatType = "Aisle";
+                    break;
+                case 6:
+                    seatType = "Window";
+                    break;
+                default:
+                    seatType = "Middle";
+                    break;
+            }
+            
+            if(i <= 12)
+            {
+                seatClass="First";
+            }
+            else if (i > 12 && i <= 30)
+            {
+                seatClass="Business";
+            }
+            else
+            {
+                seatClass="Economy";
+            }
+            seatsList.add(new Seat(null,rowNumber,seatNumber,seatClass, seatType));
+            if(seatNumber == 6)
+            {
+                seatNumber = 0;
+            }
+        }
+    }
+   
+    
+    public String ShowSeating(){
+ 
+        String txt = "";
+        
+        for(int x = 0; x < seatsList.size(); x++)
+        {
+            String checkCustomer = "";
+            if(seatsList.get(x).getCust() != null)
+            {
+                switch (seatsList.get(x).getCust().getAgeType())
+                {
+                    case "Adult":
+                        checkCustomer = "A";
+                        break;
+                    case "Child":
+                        checkCustomer = "B";
+                        break;
+                }
+            }
+            else
+            {
+                checkCustomer = "*";
+            }
+            if(seatsList.get(x).getSeatNumber() == 6)
+            {
+                txt = txt + "  " + checkCustomer + "\n";
+            }
+            else
+            {
+              txt = txt + "  " + checkCustomer;  
+            }
+        }
+        
+        return txt;
+    }
+    
+    public String SeatAllocation(Customer cust)
+    {
+        String message = "Preferred seat unavailable.";
+        for(int x = 0; x < seatsList.size(); x++)
+        {
+            if((seatsList.get(x).getSeatClass() == cust.getClassType()) && (seatsList.get(x).getSeatType() == cust.getSeatType()))
+            {
+                if(seatsList.get(x).getCust() == null)
+                {
+                    seatsList.get(x).setCust(cust);
+                    message = "Seat confirmed.";
+                    break;
+                }
+            }
+        }
+        return message;
+    }
+    
+    public String searchForCustomer(String name)
+    {
+        String message = "No matching customer found.";
+        for(int x = 0; x < seatsList.size(); x++)
+        {
+            if(seatsList.get(x).getCust().getName() == name)
+            {
+                message = "Customer Found - Row#: " + seatsList.get(x).getSeatRow() + " - Seat#: " + seatsList.get(x).getSeatNumber();
+                break;
+            }
+        }
+        return message;
+    }
+    
+    public void CancelSeatAllocation(Customer cust)
+    {
+        for(int x = 0; x < seatsList.size(); x++)
+        {
+            
+        }
     }
 }   
