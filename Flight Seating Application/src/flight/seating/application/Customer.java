37b0556274ce92f63e6bafd3deeb8c33ca68247f
NickWatt1992 <J221822@tafe.wa.edu.au>
2018-04-29 03:14:50 +0800

    Add files via upload
    Version 1.2

--- Flight Seating Application/src/flight/seating/application/Customer.java
+++ Flight Seating Application/src/flight/seating/application/Customer.java
@@ -9,20 +9,119 @@ package flight.seating.application;
  *
  * @author Nick
  */
-public class Customer {
-    String name;
-    String ageType;
-    String classType;
-    String seatType;
+
+public class Customer implements Comparable<Customer>{
+    private String _name;
+    private String _ageType;
+    private String _classType;
+    private int _seatRow;
+    private int _seatNumber;
+    private String _seatType;
+    
+  public int compareTo(Customer cust) {
+  if(this._name != null && cust._name != null){
+   return this._name.compareToIgnoreCase(cust._name);
+  }
+  return 0;
+  }
     
     public Customer()
     {
         
     }
     
-    public Customer(String name, String ageType, String classType, String seatType)
+    public Customer(String name, String ageType, String preferredClassType, int preferredSeatRow, int preferredSeatNumber, String preferredSeatType)
     {
-            
+           _name = name;
+           _ageType = ageType;
+           _classType = preferredClassType;
+           _seatRow = preferredSeatRow;
+           _seatNumber = preferredSeatNumber;
+           _seatType = preferredSeatType;
+    }
+
+    /**
+     * @return the _name
+     */
+    public String getName() {
+        return _name;
+    }
+
+    /**
+     * @param _name the _name to set
+     */
+    public void setName(String _name) {
+        this._name = _name;
+    }
+
+    /**
+     * @return the _ageType
+     */
+    public String getAgeType() {
+        return _ageType;
+    }
+
+    /**
+     * @return the _classType
+     */
+    public String getClassType() {
+        return _classType;
+    }
+
+    /**
+     * @return the _seatRow
+     */
+    public int getSeatRow() {
+        return _seatRow;
+    }
+
+    /**
+     * @return the _seatNumber
+     */
+    public int getSeatNumber() {
+        return _seatNumber;
+    }
+
+    /**
+     * @param _ageType the _ageType to set
+     */
+    public void setAgeType(String _ageType) {
+        this._ageType = _ageType;
+    }
+
+    /**
+     * @param _classType the _classType to set
+     */
+    public void setClassType(String _classType) {
+        this._classType = _classType;
+    }
+
+    /**
+     * @param _seatRow the _seatRow to set
+     */
+    public void setSeatRow(int _seatRow) {
+        this._seatRow = _seatRow;
+    }
+
+    /**
+     * @param _seatNumber the _seatNumber to set
+     */
+    public void setSeatNumber(int _seatNumber) {
+        this._seatNumber = _seatNumber;
+    }
+
+    /**
+     * @return the _seatType
+     */
+    public String getSeatType() {
+        return _seatType;
+    }
+
+    /**
+     * @param _seatType the _seatType to set
+     */
+    public void setSeatType(String _seatType) {
+        this._seatType = _seatType;
     }
     
     
--- Flight Seating Application/src/flight/seating/application/FXMLDocumentController.java
+++ Flight Seating Application/src/flight/seating/application/FXMLDocumentController.java
@@ -6,6 +6,7 @@
 package flight.seating.application;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,7 +15,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import java.util.Arrays;
+import java.util.List;
 import javafx.application.Platform;
+import javafx.scene.control.TextField;
 import javax.swing.JOptionPane;
 
 /**
@@ -22,6 +25,12 @@ import javax.swing.JOptionPane;
  * @author Nick
  */
 public class FXMLDocumentController implements Initializable {
+    
+    private Customer currentCustomer;
+
+    private FlightSeatingApplication fsa;
+    
+    private Customer foundCustomer;
     
     @FXML
     private Button seatingButton;
@@ -34,7 +43,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button businessClassButton;
     @FXML
-    private Button enconomyClassButton;
+    private Button economyClassButton;
     @FXML
     private Button windowButton;
     @FXML
@@ -44,28 +53,185 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button confirmButton;
     @FXML
-    private Button cancelButton;
-    @FXML
     public TextArea seatingTextArea;
     @FXML
     private Button exitButton;
-    
-    private FlightSeatingApplication fsa;
+    @FXML
+    public TextField firstName;
+    @FXML
+    public TextField lastName;
+    @FXML
+    private TextField searchCustomer;
+    @FXML
+    private Button searchButton;
+    @FXML
+    private Button resetButton;
+    @FXML
+    private Button removeButton;
+
     
     @FXML
     private void handleSeatingButtonAction(ActionEvent event) {
-
-        seatingTextArea.setText(fsa.ShowSeating());
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
        fsa = new FlightSeatingApplication();
+       fsa.setupSeats();
+       
+       newCustomer();
+       showSeats();
     } 
-
+    
     @FXML
     private void handleExitButtonAction(ActionEvent event) {
         Platform.exit();
     }
+    
+     @FXML
+    private void handleConfirmButtonAction(ActionEvent event) {
+        currentCustomer.setName(firstName.getText() + " " + lastName.getText());
+        fsa.customerList.add(currentCustomer);
+        fsa.SeatAllocation(currentCustomer);
+        // Add a confirmation message
+        newCustomer();
+        reset();
+        showSeats();
+        fsa.SavetoFile();
+        
+    }
+    
+    @FXML
+    private void handleResetButtonAction(ActionEvent event) {
+        reset();
+    }
+    
+    @FXML
+    private void handleSearchButtonAction(ActionEvent event) {
+        if(searchCustomer.getText().length() == 0)
+        {
+            JOptionPane.showMessageDialog(null, "Nothing entered." );
+        }
+        else
+        {
+            Customer cust = fsa.findCustomer(searchCustomer.getText());
+            if(cust != null)
+            {
+                JOptionPane.showMessageDialog(null, "Customer found." + "Customer name: " + cust.getName() + "Row#" + cust.getSeatRow() + "\n" + cust.getSeatNumber());
+                foundCustomer = cust;
+                removeButton.setDisable(false);
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Customer not found." );
+                foundCustomer = null;
+                removeButton.setDisable(true);
+            }
+            
+        }
+    }
+    
+    @FXML
+    private void handleRemoveButtonAction(ActionEvent event) {
+        removeCustomer();
+        reset();
+    }
+    
+    private void showSeats()
+    {
+        seatingTextArea.setText(fsa.ShowSeating());
+    }
+    
+    @FXML
+    private void handleAdultButtonAction(ActionEvent event) {
+        currentCustomer.setAgeType("Adult");
+        adultButton.setDisable(true);
+        childButton.setDisable(false);
+    }
+    @FXML
+    private void handleChildButtonAction(ActionEvent event) {
+        currentCustomer.setAgeType("Child");
+        adultButton.setDisable(false);
+        childButton.setDisable(true);
+    }
+    @FXML
+    private void handleFirstClassButtonAction(ActionEvent event) {
+        currentCustomer.setClassType("First");
+        firstClassButton.setDisable(true);
+        businessClassButton.setDisable(false);
+        economyClassButton.setDisable(false);
+    }
+    @FXML
+    private void handleBusinessClassButtonAction(ActionEvent event) {
+        currentCustomer.setClassType("Business");
+        firstClassButton.setDisable(false);
+        businessClassButton.setDisable(true);
+        economyClassButton.setDisable(false);
+    }
+    @FXML
+    private void handleEconomyClassButtonAction(ActionEvent event) {
+        currentCustomer.setClassType("Economy");
+        firstClassButton.setDisable(false);
+        businessClassButton.setDisable(false);
+        economyClassButton.setDisable(true);
+    }
+    @FXML
+    private void handleWindowButtonAction(ActionEvent event) {
+        currentCustomer.setSeatType("Window");
+        windowButton.setDisable(true);
+        aisleButton.setDisable(false);
+        middleButton.setDisable(false);
+    }
+    @FXML
+    private void handleAisleButtonAction(ActionEvent event) {
+        currentCustomer.setSeatType("Aisle");
+        windowButton.setDisable(false);
+        aisleButton.setDisable(true);
+        middleButton.setDisable(false);
+    }
+    @FXML
+    private void handleMiddleButtonAction(ActionEvent event) {
+        currentCustomer.setSeatType("Middle");
+        windowButton.setDisable(false);
+        aisleButton.setDisable(false);
+        middleButton.setDisable(true);
+    }
+    
+    
+    
+
+    private void setAgeType(String ageType)
+    {
+        currentCustomer.setAgeType(ageType);
+    }
+   
+    private void newCustomer()
+    {
+        currentCustomer = new Customer();
+    }
+    
+    private void removeCustomer()
+    {
+        fsa.CancelSeatAllocation(foundCustomer);
+        fsa.customers.remove(foundCustomer);
+        foundCustomer = null;
+        showSeats();
+        searchCustomer.setText("");
+    }
+    
+    private void reset()
+    {
+        firstName.setText("");
+        lastName.setText("");  
+        adultButton.setDisable(false);
+        childButton.setDisable(false);
+        firstClassButton.setDisable(false);
+        businessClassButton.setDisable(false);
+        economyClassButton.setDisable(false);
+        windowButton.setDisable(false);
+        aisleButton.setDisable(false);
+        middleButton.setDisable(false);
+    }
+    
 }
